a548c52a3b72a90d7351414577b0fa7e0871257c
LudovicLefieux <lefieux.ludovic@protonmail.com>
2023-01-08 15:40:48 +0100

    quest010

---
+++ front/src/components/SignUp.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+        }
+
+        this.updateEmailField = this.updateEmailField.bind(this);
+    }
+
+    updateEmailField(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>{ this.state.email }</h1>
+                <input 
+                    type="email" 
+                    name="email"
+                    onChange={this.updateEmailField}
+                />
+            </div>
+        );
+    }
+}
